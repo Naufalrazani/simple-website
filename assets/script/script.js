efc54b2314174d519bb28e2f7d54648e9b32b594
Naufalrazani <mnaufalrazanii@gmail.com>
2024-07-08 22:46:08 +0700

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+const nav5 = document.querySelector('nav ul li a');
+document.querySelectorAll('nav ul li a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
